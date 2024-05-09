d60a1021df2cc7f973be1d1b1b7b327aab391174
Xenhusk <desuyodavidpaul@gmail.com>
2024-05-09 18:57:35 +0800

    JavaScript

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+function DateTime(){
+    document.getElementById('demo').innerHTML = Date()
+}
+
+function Grey(){
+    document.getElementById('myImage').src='Asset 1.png'
+}
+
+function Purple(){
+    document.getElementById('myImage').src='Asset 2.png'
+}
